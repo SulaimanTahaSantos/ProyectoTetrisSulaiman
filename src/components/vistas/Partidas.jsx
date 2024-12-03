080f266347467d670685b1f99a278bbe650f1448
SulaimanTahaSantos <seltaha@sistemasmedioambientales.com>
2024-12-03 20:09:42 +0100

    Tarea 4: Reutiliza componente de tabla para Partidas

---
+++ src/components/vistas/Partidas.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import TableGames from "../TableGames";
+
+const Partidas = () => {
+    return (
+        <div>
+            <h1>Partidas</h1>
+            <TableGames />
+        </div>
+    );
+}
+
+export default Partidas;
