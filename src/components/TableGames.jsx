bbda5688d59c849115973c18a2881adeff1782cc
SulaimanTahaSantos <seltaha@sistemasmedioambientales.com>
2024-12-03 19:04:34 +0100

    Tarea 3: Implementar la funcionalidad de ordenación por columnas.

--- src/components/TableGames.jsx
+++ src/components/TableGames.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Table } from "react-bootstrap";
 
 const TableGames = () => {
@@ -9,22 +9,66 @@ const TableGames = () => {
     { id: 3, name: "aulaiman" , title: "Game 3", point: 1600, releaseDate: "2020-11-30" },
   ];
 
+  // Estado para ordenar columnas
 
+  const [sortColumns, setSortColumns] = useState({
+    key: "id",
+    direction: "ascending",
+  });
 
-  return (
+    // Función para ordenar columnas
+    const handleSort = (key) => {
+        let direction = "ascending";
+        if (sortColumns.key === key && sortColumns.direction === "ascending") {
+          direction = "descending";
+        }
+        setSortColumns({ key, direction });
+    }
+
+    // ordenar las filas segun la key
+
+    const sortedGames = [...partidas].sort((a,b)=>{
+        if(a[sortColumns.key] < b[sortColumns.key]){
+            return sortColumns.direction === "ascending" ? -1 : 1;
+        }
+        if(a[sortColumns.key] > b[sortColumns.key]){
+            return sortColumns.direction === "ascending" ? 1 : -1;
+        }
+        return 0;
+        
+    })
+
+      const getArrow = (key) => {
+    if (sortColumns.key === key) {
+      return sortColumns.direction === 'ascending' ? '↑' : '↓';
+    }
+    return '';
+  };
+
+   return (
     <div>
-      <h1>Game List</h1>
       <Table striped bordered hover>
         <thead>
           <tr>
-            <th>ID</th>
-            <th>Title</th>
-            <th>Genre</th>
-            <th>Release Date</th>
+            <th onClick={() => handleSort('id')}>
+              ID {getArrow('id')}
+            </th>
+            <th onClick={() => handleSort('name')}>
+              Name {getArrow('name')}
+            </th>
+            <th onClick={() => handleSort('title')}>
+              Games {getArrow('title')}
+            </th>
+            <th onClick={() => handleSort('point')}>
+              Points {getArrow('point')}
+            </th>
+            <th onClick={() => handleSort('releaseDate')}>
+              Release Date {getArrow('releaseDate')}
+            </th>
           </tr>
         </thead>
         <tbody>
-          {partidas.map((game) => (
+          {sortedGames.map((game) => (
             <tr key={game.id}>
               <td>{game.id}</td>
               <td>{game.name}</td>
