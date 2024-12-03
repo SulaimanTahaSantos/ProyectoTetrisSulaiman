3e02a65fdad37480f62cf685398544a627c00255
SulaimanTahaSantos <seltaha@sistemasmedioambientales.com>
2024-12-03 20:02:17 +0100

    Tarea 2: Crear el componente para la vista Inicio con las instrucciones.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import TableGames from './components/TableGames'
+import Inicio from './components/vistas/Inicio'
 
 function App() {
  
 
   return (
     <>
- <TableGames />
+ {/* <TableGames /> */}
+ <Inicio />
     </>
   )
 }
