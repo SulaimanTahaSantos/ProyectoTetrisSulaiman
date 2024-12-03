ca4b797199a45c58930f416fc9f7172ddf0abbf3
SulaimanTahaSantos <seltaha@sistemasmedioambientales.com>
2024-12-03 18:28:13 +0100

    Desplegar arreglado

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    build: {
+        outDir: 'dist', // Esto indica que Vite generará los archivos de producción en la carpeta 'dist'
+    },
+    plugins: [react()],
 })
