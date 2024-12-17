c9ed43dd8a1571aceed63000097e43ea11b48676
SulaimanTahaSantos <seltaha@sistemasmedioambientales.com>
2024-12-17 19:50:49 +0100

    Arreglando algunas cosas

--- src/components/vistas/Juego.jsx
+++ src/components/vistas/Juego.jsx
@@ -3,16 +3,34 @@ import { Container } from 'react-bootstrap';
 import AppMenu from '../AppMenu';
 
 const Juego = () => {
-    return (
-
-        <>
-        <AppMenu />
-           <Container>
+  return (
+    <>
+      <AppMenu />
+      <Container>
         <h1 className='mt-5'>Juego</h1>
-        </Container>
-        </>
-     
-    );
-}
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <div
+            style={{
+              width: '300px',
+              height: '600px',
+              border: '2px solid black',
+              backgroundColor: 'lightgray',
+              
+            }}
+          >
+            {/* Aquí mostraremos la lógica del juego */}
+           
+          </div>
+        </div>
+      </Container>
+    </>
+  );
+};
 
 export default Juego;
