277a5ac306a2f2df477c9dc4bf0be63c987c74bf
SulaimanTahaSantos <seltaha@sistemasmedioambientales.com>
2025-02-16 21:11:21 +0000

    arreglado redireccion en produccion

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig({
-    build: {
-        outDir: 'dist', // Esto indica que Vite generará los archivos de producción en la carpeta 'dist'
-    },
-    plugins: [react()],
-})
+  build: {
+    outDir: 'dist', // Genera los archivos de producción en la carpeta 'dist'
+  },
+  plugins: [react()],
+  server: {
+    historyApiFallback: true // Para desarrollo; redirige rutas desconocidas a index.html
+  }
+});
